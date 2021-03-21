75f16f5e458245b2fe46e1cfda5803731107aaa1
Samratkunwar <samratkunwar12@gmail.com>
2021-03-22 00:10:45 +1000

    deployment trial-5

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import HomePage from './Pages/Home/home.page';
 import ProfilePage from './Pages/Profile/profile.page';
 
 // import Component
-import NavMenu from './Components/MenuComponents/NavMenu/NavMenu';
+// import NavMenu from './Components/MenuComponents/NavMenu/NavMenu';
 import ProjectPage from './Pages/Project/project.page';
 import Landingpage from './Pages/LangingPage/Landingpage';
 import AboutMe from './Pages/AboutMe/AboutMe';
@@ -19,13 +19,13 @@ import AboutMe from './Pages/AboutMe/AboutMe';
 function App() {
   return (
     <div className="App">
-      <NavMenu />
-      <Route path='/' component={Landingpage} exact />
-      <Route path='/home' component={HomePage} exact />
-      <Route path='/profile' component={ProfilePage} exact />
-      <Route path='/aboutme' component={AboutMe} exact />
-      <Route path='/projects' component={ProjectPage} exact />
-      <Route path='/project/:_id' component={ProjectPage} exact />
+      {/* <NavMenu /> */}
+      <Route path='/ePortfolio/' component={Landingpage} exact />
+      <Route path='/ePortfolio/home' component={HomePage} exact />
+      <Route path='/ePortfolio/profile' component={ProfilePage} exact />
+      <Route path='/ePortfolio/aboutme' component={AboutMe} exact />
+      <Route path='/ePortfolio/projects' component={ProjectPage} exact />
+      <Route path='/ePortfolio/project/:_id' component={ProjectPage} exact />
 
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
