b9cf0f0fb0a8d7a5dbe84bf36d67fdfd56022906
Samratkunwar <samratkunwar12@gmail.com>
2021-03-21 23:25:03 +1000

    Updates

---
+++ precache-manifest.928cc6c65c5ef68f9e9992553e19bbd2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea94c278e43140013c8e004089533e83",
+    "url": "/ePortfolio/index.html"
+  },
+  {
+    "revision": "f8b5572097cad2ed7e3a",
+    "url": "/ePortfolio/static/css/main.de0a4561.chunk.css"
+  },
+  {
+    "revision": "539328a9cfa28d9e070d",
+    "url": "/ePortfolio/static/js/2.7cee1cae.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/ePortfolio/static/js/2.7cee1cae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8b5572097cad2ed7e3a",
+    "url": "/ePortfolio/static/js/main.d82518d7.chunk.js"
+  },
+  {
+    "revision": "22f83c27af9eea4a2071",
+    "url": "/ePortfolio/static/js/runtime-main.155e6c0b.js"
+  },
+  {
+    "revision": "84c0548bd0755a02f20a91bdde411593",
+    "url": "/ePortfolio/static/media/illustration1.84c0548b.svg"
+  },
+  {
+    "revision": "c159867d8385f4b4a739e3d0b1928f88",
+    "url": "/ePortfolio/static/media/list.c159867d.svg"
+  },
+  {
+    "revision": "0e3fe0b76bdde41c6a79cb4ee19378ac",
+    "url": "/ePortfolio/static/media/photo1.0e3fe0b7.jpg"
+  },
+  {
+    "revision": "d4092dcd3b71d99387d646fccafecf8b",
+    "url": "/ePortfolio/static/media/photo2.d4092dcd.jpg"
+  },
+  {
+    "revision": "fecc9d46d63e0cce79f53dcbe5ca722a",
+    "url": "/ePortfolio/static/media/photo3.fecc9d46.jpg"
+  },
+  {
+    "revision": "ccc4546ec92ef92e1566bd7a74f868c5",
+    "url": "/ePortfolio/static/media/photo4.ccc4546e.jpg"
+  }
+]);
